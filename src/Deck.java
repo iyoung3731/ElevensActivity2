59671e98f965d5a873ca1adfa4a0171d5823a34c
IvyYoung <iyoung3731@bths.edu>
2019-03-11 18:46:13 -0400

    b

--- src/Deck.java
+++ src/Deck.java
@@ -34,6 +34,8 @@ public class Deck {
      */
     public Deck(String[] ranks, String[] suits, int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        cards = new ArrayList<Card>();
+
         for(int i=0; i<ranks.length; i++){
             for(int j=0; j<suits.length; j++){
                 cards.add(new Card(ranks[i],suits[j],pointValues[i]));
